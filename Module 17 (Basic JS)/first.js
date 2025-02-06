685990504d035b24f61943e3efad4745cd84207e
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-02-06 16:11:25 +0600

    first commit

---
+++ Module 17 (Basic JS)/first.js
@@ -0,0 +1,18 @@
+console.log(22)
+
+var orangePrice = 50;
+var applePrice = 20;
+var tewnty = '20'
+
+console.log(orangePrice)
+console.log(typeof tewnty)
+console.log(parseFloat(tewnty)) //convert string to number
+
+console.log(orangePrice+applePrice)
+console.log(typeof orangePrice)
+
+var first = 0.1
+var second = 0.2
+var total = first+second
+console.log(total)
+console.log(total.toFixed(4)) //Number of decimal digits.
