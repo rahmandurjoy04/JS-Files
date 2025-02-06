00f269ad9f2d816e76e8503c68d22bb5aa828a07
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-02-06 19:45:11 +0600

    Array Declared

---
+++ Module_18.5_Fundamental_concepts_of_Array/1.declaring_array.js
@@ -0,0 +1,14 @@
+//Declaring the array
+const fruits = ['Aam', 'Jaam', 'Kola', 'Pepe', 'Anarosh']
+
+//Showing the full array
+console.log(fruits)
+
+//Showing the third index element
+console.log(fruits[2])
+
+//Changing the second index by 'Jambura'
+fruits[1]='Jambura'
+
+//Showing the modified array
+console.log(fruits)
