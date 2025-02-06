853bac3be4965a694c69d671226bcacb858b11bf
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-02-06 16:42:11 +0600

    Nasted Friend Added

---
+++ Module_18_Conditions_In_JavaScript /4.Nasted_Friend.js
@@ -0,0 +1,23 @@
+let myMarks = 85;
+let friendsMark =36;
+
+if (myMarks > 80){
+    if(friendsMark >= 80){
+        console.log("We'll go for a lunch!")
+    }
+
+    else if(friendsMark >= 60 && friendsMark <80){
+        console.log("Good luck next time...Bondhu!")
+    }
+
+    else if(friendsMark >= 40 && friendsMark <60){
+        console.log("I'll keep his message unseen!")
+    }
+
+    else{
+        console.log("Yor're Blocked Bondhu...!")
+    }
+}
+else {
+    console.log("I'll sleep after going home & also act sad")
+}
