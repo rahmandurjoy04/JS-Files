9b56dcb2343064afa632b75904392ffacb7a7daa
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-02-06 16:40:31 +0600

    Grade Calculator Added

---
+++ Module_18_Conditions_In_JavaScript /3.Grade_Claculator.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')();
+
+console.log("Welcome to Grade Calculator");
+
+let marks = parseInt(prompt("Enter Your Marks : " ))
+console.log("Mark you entered is : "+marks)
+        // document.write("Your Mark is "+marks)
+
+        if(marks >=90 && marks <= 100){
+            console.log("You got A")
+        }
+
+        else if (marks >= 80 && marks <=89){
+            console.log("You got B")
+        }
+
+        else if(marks >= 70 && marks <=79){
+            console.log("You got C")
+        }
+
+        else if(marks >= 60 && marks <=69){
+            console.log("You got C")
+        }
+
+        else if(marks >= 0 && marks <=59){
+            console.log("You got F")
+        }
