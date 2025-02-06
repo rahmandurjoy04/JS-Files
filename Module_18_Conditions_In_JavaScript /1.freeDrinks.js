f3b8e31e6d0359f71c2c8f72affc54f4981cdea7
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-02-06 16:28:09 +0600

    Free drinks Added

---
+++ Module_18_Conditions_In_JavaScript /1.freeDrinks.js
@@ -0,0 +1,10 @@
+let burgerPrice = 501;
+if (burgerPrice > 500){
+    console.log("You'll get a free coke!")
+}
+else{
+    console.log("Coke price 30 TK")
+}
+
+/* this condition using ternary opeartor */
+burgerPrice > 500 ? console.log("You'll get a free coke!") : console.log("Coke price 30 TK")
