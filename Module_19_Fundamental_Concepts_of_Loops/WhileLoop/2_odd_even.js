bac18b1f3ddac493db951f19d8e85028b4fd2599
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-02-09 20:48:30 +0600

    While loops added

---
+++ Module_19_Fundamental_Concepts_of_Loops/WhileLoop/2_odd_even.js
@@ -0,0 +1,27 @@
+/*
+Subtask-1:
+Find all the odd numbers from 61 to 100.
+*/
+console.log("All the odd numbers from 61 to 100 are : ")
+let i = 61;
+while (i <= 100){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+    i++
+}
+
+
+/*
+Subtask-2:
+Find all the even numbers from 78 to 98.
+*/
+console.log("All the even numbers from 78 to 98 are : ")
+
+let j = 78;
+while(j <= 98){
+    if(j % 2 ===0){
+        console.log(j);
+    }
+    j++;
+}
---
+++ Module_19_Fundamental_Concepts_of_Loops/WhileLoop/4_multiplication_table.js
@@ -0,0 +1,11 @@
+/*
+As Ersa is learning now, 
+she wants to explore more and more. 
+Tell Ersa to generate a multiplication table for number 5
+*/
+let table = 5;
+let i = 1;
+while(i <= 10){
+    console.log(table ,"*", i, "=" ,table*i);
+    i++;
+} 
